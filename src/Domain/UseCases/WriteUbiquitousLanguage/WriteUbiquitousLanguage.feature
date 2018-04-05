f82d8aec00d220a1ff575ced23a0f05334d71468
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-04-05 07:43:55 -0300

    write ubiquitous language into files

---
+++ src/Domain/UseCases/WriteUbiquitousLanguage/WriteUbiquitousLanguage.feature
@@ -0,0 +1,21 @@
+Feature: Write Ubiquitous Language
+  As a use case
+  I want translate domain entities into source files
+  In order to be able to use them
+
+  Scenario Outline: Known translator
+    Given the <translator> translator
+      And a pattern
+      And some output
+      And set writer
+      And a pen
+    When I call WriteUbiquitousLanguage
+    Then writter is invoked
+      And translation was precise
+      And pen was used
+      And a filePath was provided
+
+    Examples:
+      | translator |
+      | "ubi" |
+      | "log" |
---
+++ src/Domain/UseCases/WriteUbiquitousLanguage/WriteUbiquitousLanguage.steps.js
@@ -0,0 +1,70 @@
+const assert = require('assert')
+const fs = require('fs')
+const { Given, When, Then } = require('cucumber')
+const { mock, match } = require('sinon')
+const { lorem } = require('faker')
+
+const WriteUbiquitousLanguage = require('./')
+
+Given('the {string} translator', function (translator) {
+  this.args = Object.assign({}, this.args, { translator })
+})
+
+Given('a pattern', function () {
+  this.args = Object.assign({}, this.args, {
+    pattern: `${__dirname}/fixture/file.fixture`
+  })
+})
+
+Given('some output', function () {
+  this.args = Object.assign({}, this.args, {
+    output: lorem.word()
+  })
+})
+
+Given('set writer', function () {
+  this.aux = Object.assign({}, this.aux, {
+    writer: mock('writer')
+  })
+})
+
+Given('a pen', function () {
+  this.injection = Object.assign({}, this.injection, {
+    pen: lorem.word()
+  })
+})
+
+When('I call WriteUbiquitousLanguage', async function () {
+  const { writer } = this.aux
+
+  writer.withExactArgs(match.object)
+
+  this.result = await WriteUbiquitousLanguage.call({ writer }, this.args, this.injection)
+})
+
+Then('writter is invoked', function () {
+  this.aux.writer.verify()
+})
+
+Then('translation was precise', function () {
+  const filePath = `${__dirname}/fixture/file.${this.args.translator}.fixture`
+
+  const [ [ { entity } ] ] = this.aux.writer.args
+  const expectation = fs.readFileSync(filePath, 'utf8')
+
+  assert.equal(entity, expectation.trim())
+})
+
+Then('pen was used', function () {
+  const [ [ { pen } ] ] = this.aux.writer.args
+
+  assert.equal(pen, this.injection.pen)
+})
+
+Then('a filePath was provided', function () {
+  const [ [ { filePath } ] ] = this.aux.writer.args
+
+  const expectation = `${this.args.output}/File`
+
+  assert.ok(filePath.includes(expectation))
+})
