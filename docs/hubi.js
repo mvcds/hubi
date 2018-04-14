f92f46175f193f885fd84e063e430d299d917324
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-04-14 18:29:21 -0300

    allow to open all
    add early return

--- docs/hubi.js
+++ docs/hubi.js
@@ -1,14 +1,20 @@
 //  eslint-disable-next-line
 const j = $
 
+function open (detail) {
+  detail.prop('open', true)
+}
+
 j(document).ready(function () {
   var hash = window.location.hash.substr(1)
 
   if (!hash.length) return
 
+  if (hash === 'all') return open(j('details'))
+
   const token = j('[name=' + hash + ']')
 
   if (!token) return
 
-  j(token).prop('open', true)
+  open(j(token))
 })
