2c83d763c70de9fb97d65980c8dbf39961ed697e
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-05-11 10:56:07 -0300

    add TODO

--- src/Domain/Objects/AttributeParser/AttributeParser.test.js
+++ src/Domain/Objects/AttributeParser/AttributeParser.test.js
@@ -76,6 +76,7 @@ describe('AttributeParser', () => {
     })
   })
 
+  //  TODO: fix intermitence
   context.skip('Deprecation', () => {
     const testDeprecation = ({ deprecated }, message, error) => {
       it('Matches the message', () => assert.equal(deprecated.message, message))
