27db83dd4d371a1aaeb05dc1ece42abac06e2c12
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-12-23 11:25:09 -0200

    fix coverage

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # this file is necessary because nyc was not running coverage properly
 # https://stackoverflow.com/q/45454462/3178998
-yarn test:unit
-yarn test:integration
+npm run test:unit
+npm run test:integration
