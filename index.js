86b1602202ee87afbce1c45f51b6823d626fbea9
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2020-01-21 19:47:30 +0100

    chore: create domain layer

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const save = require('./src/Domain/UseCases/SaveUbiquitousLanguageIntoFile')
-const log = require('./src/Domain/UseCases/LogUbiquitousLanguageIntoConsole')
+const save = require('./src/0-domain/UseCases/SaveUbiquitousLanguageIntoFile')
+const log = require('./src/0-domain/UseCases/LogUbiquitousLanguageIntoConsole')
 
 const Hubi = {
   log,
